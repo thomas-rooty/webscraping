d35c71cef860ff5659bdb814d644016df3b02409
thomas.caron <tc.caron91@gmail.com>
2023-12-11 15:08:08 +0100

    Download search results as CSV

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import streamlit as st
 from functions import *
+import streamlit as st
 import pandas as pd
 import bs4
 
@@ -14,10 +14,13 @@ st.title("Capybar'App")
 # Text form
 search_param = st.text_input('Rechercher sur BDM', 'IA, Adobe, Tech, Dev, ...')
 
+# Number form
+page_nb = str(st.number_input('Page', min_value=1, max_value=100, value=1))
+
 # Search articles
 if st.button('Search'):
   # Generate search result
-  search_result = search_on_bdm(search_param, '1')
+  search_result = search_on_bdm(search_param, page_nb)
   st.write('Status code: ', search_result.status_code)
   st.write('URL: ', search_result.url)
 
@@ -26,3 +29,6 @@ if st.button('Search'):
   json = build_json(soup)
   df = pd.DataFrame.from_dict(json, orient='index')
   st.write(df)
+
+  # Download CSV
+  st.markdown(download_csv(df), unsafe_allow_html=True)
--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
+import base64
 import requests
+
 
 # Fetch the BDM website using a search parameter
 def search_on_bdm(param, page):
@@ -9,7 +11,16 @@ def build_json(search_result):
   return {
     article.get('id'): {
       'name': article.find('h3').text if article.find('h3') else 'No name available',
+      'link': article.find('a').get('href') if article.find('a') else 'No link available',
+      'img': article.find('img').get('src') if article.find('img') else 'No image available',
       'category': article.find('span').text if article.find('span') else 'No category available',
       'date': article.find('time').text if article.find('time') else 'No date available',
     } for article in search_result.find_all('article')
   }
+
+# Function that downloads the dataframe as a CSV file
+def download_csv(df):
+  csv = df.to_csv(index=False)
+  b64 = base64.b64encode(csv.encode()).decode()
+  href = f'<a href="data:file/csv;base64,{b64}" download="bdm.csv">Download CSV file</a>'
+  return href
