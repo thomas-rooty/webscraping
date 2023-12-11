6159d0b3ebdc39821f3ccb9ed127fe8b97e76ab7
thomas.caron <tc.caron91@gmail.com>
2023-12-11 15:54:43 +0100

    Fixed page association

--- beautiful.py
+++ beautiful.py
@@ -2,20 +2,21 @@ import streamlit as st
 import pandas as pd
 import json
 
-# Sample JSON data (replace with your JSON data)
-data = pd.read_json('dataframe.json').to_json(orient='records')
+def show_beautiful():
+  # Sample JSON data (replace with your JSON data)
+  data = pd.read_json('dataframe.json').to_json(orient='records')
 
-# Parse the JSON into a DataFrame
-data_dict = json.loads(data)
-df = pd.DataFrame(data_dict)
+  # Parse the JSON into a DataFrame
+  data_dict = json.loads(data)
+  df = pd.DataFrame(data_dict)
 
-# Streamlit page layout
-st.title("Latest Tech Articles")
+  # Streamlit page layout
+  st.title("Latest Tech Articles")
 
-for index, row in df.iterrows():
-  st.subheader(row['name'])
-  st.image(row['img'], width=100)
-  st.write(f"Category: {row['category']}")
-  st.write(f"Published on: {row['date']}")
-  st.markdown(f"[Read More]({row['link']})", unsafe_allow_html=True)
-  st.write("---")  # Separator line
+  for index, row in df.iterrows():
+    st.subheader(row['name'])
+    st.image(row['img'], width=100)
+    st.write(f"Catégorie: {row['category']}")
+    st.write(f"Publié le: {row['date']}")
+    st.markdown(f"[Voir l'article original]({row['link']})", unsafe_allow_html=True)
+    st.write("---")  # Separator line
