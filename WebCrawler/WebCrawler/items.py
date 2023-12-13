bf2a2087418a2ca6f49eda4abe0b7340bd5f8f1e
thomas.caron <tc.caron91@gmail.com>
2023-12-13 10:31:04 +0100

    Resolved imports and added clean_csv function to items

--- WebCrawler/WebCrawler/items.py
+++ WebCrawler/WebCrawler/items.py
@@ -1,12 +1,21 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class WebcrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+# À ajouter au fichier items.py
+class ReviewsAllocineItem(scrapy.Item):
+    title = scrapy.Field()
+    img = scrapy.Field()
+    author = scrapy.Field()
+    time = scrapy.Field()
+    genre = scrapy.Field()
+    score = scrapy.Field()
+    desc = scrapy.Field()
+    release = scrapy.Field()
+
+    def clean_item(self):
+        for key, value in self.items():
+            if isinstance(value, str):
+                self[key] = value.strip().replace('\n', '')
+            elif isinstance(value, list):
+                self[key] = [item.strip() for item in value]
+        return self
--- WebCrawler/WebCrawler/spiders/allocine.py
+++ WebCrawler/WebCrawler/spiders/allocine.py
@@ -1,10 +1,72 @@
 import scrapy
+from scrapy import Request
+from ..items import ReviewsAllocineItem
 
 
 class AllocineSpider(scrapy.Spider):
-    name = "allocine"
-    allowed_domains = ["www.allocine.fr"]
-    start_urls = ["https://www.allocine.fr/film/meilleurs"]
+    name = 'allocine'
+    allowed_domains = ['www.allocine.fr']
 
-    def parse(self, response):
-        pass
+    # Liste des pages à collecter
+    start_urls = [f'https://www.allocine.fr/film/meilleurs/?page={n}' for n in range(1, 10)]
+
+    def start_requests(self):
+        for url in self.start_urls:
+            yield Request(url=url, callback=self.parse_allocine)
+
+    def parse_allocine(self, response):
+        liste_film = response.css('li.mdl')
+
+        # Boucle qui parcours l'ensemble des éléments de la liste des films
+        for film in liste_film:
+            item = ReviewsAllocineItem()
+
+            # Nom du film
+            try:
+                item['title'] = film.css('a.meta-title-link::text').get()
+            except:
+                item['title'] = 'None'
+
+            # Lien de l'image du film
+            try:
+                item['img'] = film.css('img.thumbnail-img').attrib['src']
+            except:
+                item['img'] = 'None'
+
+            # Auteur du film
+            try:
+                item['author'] = film.css('a.blue-link::text').get()
+            except:
+                item['author'] = 'None'
+
+            # Durée du film
+            try:
+                item['time'] = film.css('div.meta-body-info::text').get()
+            except:
+                item['time'] = 'None'
+
+            # Genre cinématographique
+            try:
+                item['genre'] = film.css('div.meta-body-item span::text').getall()
+            except:
+                item['genre'] = 'None'
+
+            # Score du film
+            try:
+                item['score'] = film.css('span.stareval-note::text').get()
+            except:
+                item['score'] = 'None'
+
+            # Description du film
+            try:
+                item['desc'] = film.css('div.synopsis div.content-txt::text').get()
+            except:
+                item['desc'] = 'None'
+
+            # Date de sortie
+            try:
+                item['release'] = film.css('span.date::text').get()
+            except:
+                item['release'] = 'None'
+
+            yield item.clean_item()
