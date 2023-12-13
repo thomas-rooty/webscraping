e24b2cb3126f7b9fcb48829c24006cae12a8567c
thomas.caron <tc.caron91@gmail.com>
2023-12-13 14:11:42 +0100

    Storing indices in DB

--- WebCrawler/WebCrawler/pipelines.py
+++ WebCrawler/WebCrawler/pipelines.py
@@ -6,8 +6,57 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import sqlalchemy as db
 
 
 class WebcrawlerPipeline:
     def process_item(self, item, spider):
         return item
+
+
+class DataBase():
+    def __init__(self, name_database='database'):
+        self.name = name_database
+        self.url = f"sqlite:///{name_database}.db"
+        self.engine = db.create_engine(self.url)
+        self.connection = self.engine.connect()
+        self.metadata = db.MetaData()
+        self.table = self.engine.table_names()
+
+    def create_table(self, name_table, **kwargs):
+        colums = [db.Column(k, v, primary_key=True) if 'id_' in k else db.Column(k, v) for k, v in kwargs.items()]
+        db.Table(name_table, self.metadata, *colums)
+        self.metadata.create_all(self.engine)
+        print(f"Table : '{name_table}' are created succesfully")
+
+    def read_table(self, name_table, return_keys=False):
+        table = db.Table(name_table, self.metadata, autoload=True, autoload_with=self.engine)
+        if return_keys:
+            table.columns.keys()
+        else:
+            return table
+
+    def add_row(self, name_table, **kwarrgs):
+        name_table = self.read_table(name_table)
+
+        stmt = (
+            db.insert(name_table).
+            values(kwarrgs)
+        )
+        self.connection.execute(stmt)
+        print(f'Row id added')
+
+    def delete_row_by_id(self, table, id_):
+        name_table = self.read_table(name_table)
+
+        stmt = (
+            db.delete(name_table).
+            where(students.c.id_ == id_)
+        )
+        self.connection.execute(stmt)
+        print(f'Row id {id_} deleted')
+
+    def select_table(self, name_table):
+        name_table = self.read_table(name_table)
+        stm = db.select([name_table])
+        return self.connection.execute(stm).fetchall()
--- WebCrawler/WebCrawler/spiders/boursorama.py
+++ WebCrawler/WebCrawler/spiders/boursorama.py
@@ -2,6 +2,7 @@ import scrapy
 from scrapy import Request
 from ..items import ReviewsBoursoramaItem
 from time import gmtime, strftime
+from ..pipelines import *
 
 
 class BoursoramaSpider(scrapy.Spider):
@@ -9,6 +10,20 @@ class BoursoramaSpider(scrapy.Spider):
     allowed_domains = ['finance.yahoo.com']
     start_urls = [f'https://www.boursorama.com/bourse/actions/palmares/france/page-{n}?france_filter%5Bmarket%5D=1rPCAC'
                   for n in range(1, 10)]
+    database = DataBase('boursorama')
+
+    try:
+        database.create_table('boursorama',
+                              nomIndice=db.String,
+                              coursAction=db.String,
+                              variationAction=db.String,
+                              ath=db.String,
+                              atl=db.String,
+                              valeurOuverture=db.String,
+                              collectDatetime=db.String
+                              )
+    except:
+        pass
 
     def start_requests(self):
         for url in self.start_urls:
@@ -20,46 +35,49 @@ class BoursoramaSpider(scrapy.Spider):
         for indices in liste_indices:
             item = ReviewsBoursoramaItem()
 
-            #indice boursier
+            # indice boursier
             try:
                 item['nomIndice'] = indices.css('a::text').get()
             except:
                 item['nomIndice'] = 'None'
 
-            #indice cours de l'action
+            # indice cours de l'action
             try:
                 item['coursAction'] = indices.css('span.c-instrument--last::text').get()
             except:
                 item['coursAction'] = 'None'
 
-            #Variation de l'action
+            # Variation de l'action
             try:
                 item['variationAction'] = indices.css('span.c-instrument--instant-variation::text').get()
             except:
                 item['variationAction'] = 'None'
 
-            #Valeur la plus haute
+            # Valeur la plus haute
             try:
                 item['ath'] = indices.css('span.c-instrument--high::text').get()
             except:
                 item['ath'] = 'None'
 
-            #Valeur la plus basse
+            # Valeur la plus basse
             try:
                 item['atl'] = indices.css('span.c-instrument--low::text').get()
             except:
                 item['atl'] = 'None'
 
-            #Valeur d'ouverture
+            # Valeur d'ouverture
             try:
                 item['valeurOuverture'] = indices.css('span.c-instrument--open::text').get()
             except:
                 item['valeurOuverture'] = 'None'
 
-            #Date de la collecte
+            # Date de la collecte
             try:
                 item['collectDatetime'] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
             except:
                 item['collectDatetime'] = 'None'
+
+            # Store data in database
+            self.database.add_row('boursorama', **item)
 
             yield item
